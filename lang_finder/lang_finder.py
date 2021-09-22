79928971e6bc4466a2d15da91097f40bfe8f58e8
TK <71311548+microcoder-py@users.noreply.github.com>
2021-09-22 12:09:29 +0530

    Update lang_finder.py

--- lang_finder/lang_finder.py
+++ lang_finder/lang_finder.py
@@ -1,14 +1,24 @@
 import tensorflow as tf
 import tensorflow_text as text
 
+# <--------- Loading Model --------->
+# Please note that the model was too large to upload to GitHub,
+# I have shared the download link for it in the Readme.md file
+# Download and place it in the same file hierarchy as this file
 model = tf.keras.models.load_model('language_detector')
 
+# <--------- Languages --------->
+# Please note that if there is any change to the list of languages in 
+# modelling.py, update this list with the same changes
 list_languages = [
     "en",  "ar", "de", "it", "ko", "bg", "da", "el", "fa", "fi", "id", "no", "ro", "sk", "sl", "tl"
     ]
 
 list_languages.sort()
 
+# <--------- Function to run inference on model --------->
+# Nothing too complex, it accepts a list of strings,
+# returns a list of languages
 def find_language(documents):
     documents = tf.constant(documents, tf.string)
     class_probability = model(documents)
